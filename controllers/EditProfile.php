6ec97b613fd7aa6a78f75de46d5c5bb80cffc11b
Vincent2703 <vincent.bourdon41@gmail.com>
2023-08-14 15:33:19 +0200

    Mise à jour du 14/08/2023
    + Debut refonte complète du système des champs personnalisés pour les dossiers logement (options, editProfile et editHF)

--- controllers/EditProfile.php
+++ controllers/EditProfile.php
@@ -7,106 +7,127 @@ class REALM_EditProfile {
         require_once(PLUGIN_RE_PATH."models/UserModel.php");
         
         $role = $userWP->roles[0];        
-        $agencies = REALM_UserModel::getUsersByRole("agency");
+        //$agencies = REALM_UserModel::getUsersByRole("agency");
         
         $user = REALM_UserModel::getUser($userWP->ID);
+        
+        if(defined("PLUGIN_REP_NAME") && $role === "customer") { 
+            $customerCustomFields = json_decode(get_option(PLUGIN_REP_NAME."Options")["customFields"], true);
+            
+            $fields = array(
+                "buyer" => array(
+                    "personalSituation" => array(),
+                    "professionalSituation" => array(),
+                    "other" => array()
+                ),
+                "guarantor" =>  array(
+                    "personalSituation" => array(),
+                    "professionalSituation" => array(),
+                    "other" => array()
+                )
+            );
+                   
+            foreach($customerCustomFields as $CF) {
+                if($CF["forWhom"] === "buyer" || $CF["forWhom"] === "guarantor") {
+                    array_push($fields[$CF["forWhom"]][$CF["category"]], $CF);
+                }else{
+                    array_push($fields["buyer"][$CF["category"]], $CF);
+                    array_push($fields["guarantor"][$CF["category"]], $CF);
+                }
+            }
+            
         ?>
-        <h3 id="extraInformationTitle"><span id="roleName"><?= ucfirst($role);?></span></h3>
-        <input type="hidden" name="nickname" value="<?=$user["displayName"];?>">
-            <table class="form-table" id="extraInformation">
-                <tr class="form-field customer">
-                    <th scope="row">
-                        <label for="customerPhone"><?php _e("Phone", "reptxtdom");?></label>
-                    </th>
-                    <td>
-                        <input type="text" name="customerPhone" id="customerPhone" class="regular-text" value="<?=$user["customerPhone"];?>">
-                    </td>
-                </tr>
-                <?php if($role === "customer") {
-                foreach($user["customFields"] as $kField => $vField) { ?>
-                <tr class="form-field customer">
-                    <th scope="row">
-                        <label for="CF<?=$vField["nameAttr"];?>"><?=$kField;?></label>
-                    </th>
-                    <td>
-                    <?php if($vField["type"] === "text") { ?>
-                        <input type="text" id="CF<?=$vField["nameAttr"];?>" name="CF<?=$vField["nameAttr"];?>" class="regular-text" value="<?=$vField["value"];?>">
-                    <?php }else if($vField["type"] === "file") { ?>
-                        <input type="file" name="CF<?=$vField["nameAttr"];?>" accept="<?=$vField["extensions"];?>"<?=$vField["optionnal"]?'':" required";?>>
-                        <?php if(isset($vField["file"]) && is_array($vField["file"]) && !empty($vField["file"])) { ?>
-                        <a target="_blank" href="<?=$vField["file"]["url"];?>"><?=$vField["file"]["name"];?></a>
-                        <?php }
-                        }
-                    } ?>
-                    </td>
-                </tr>
-                <?php } ?>
-                <tr class="form-field agent">
-                    <th scope="row">
-                        <label for="agentPhone"><?php _e("Phone", "retxtdom");?></label>
-                    </th>
-                    <td>
-                        <input type="text" name="agentPhone" id="agentPhone" class="regular-text" value="<?=$user["agentPhone"];?>">
-                    </td>
-                </tr>
-                <tr class="form-field agent">
-                    <th scope="row">
-                         <label for="agentMobilePhone"><?php _e("Mobile phone", "retxtdom");?></label>
-                    </th>
-                    <td>
-                        <input type="text" name="agentMobilePhone" id="agentMobilePhone" class="regular-text" value="<?=$user["agentMobilePhone"];?>">
-                    </td>
-                </tr>
-                <tr class="form-field agent">
-                    <th scope="row">
-                        <label for="agentAgency"><?php _e("Agency", "retxtdom");?></label>
-                    </th>
-                    <td>
-                        <select name="agentAgency" id="agencies">
-                        <?php
-                            foreach($agencies as $agency) {
-                                ?>
-                                <option value="<?= $agency->ID; ?>" <?php selected($agency->ID, $user["agentAgency"]); ?>><?= $agency->display_name; ?></option>
-                                <?php
-                            }
-                        ?>
-                        </select>
-                    </td>
-                </tr>
-                <tr class="form-field agency">
-                    <th scope="row">
-                        <label for="agencyPhone"><?php _e("Phone", "retxtdom");?></label>
-                    </th>
-                    <td>
-                        <input type="text" name="agencyPhone" id="agencyPhone" class="regular-text" value="<?=$user["agencyPhone"];?>">
-                    </td>
-                </tr>
-                <tr class="form-field agency">
-                    <th scope="row">
-                         <label for="agencyAddress"><?php _e("Address", "retxtdom");?></label>
-                    </th>
-                    <td>
-                        <input type="text" name="agencyAddress" id="agencyAddress" class="regular-text" value="<?=$user["agencyAddress"];?>">
-                    </td>
-                </tr>
-                <tr class="form-field agency">
-                    <th scope="row">
-                         <label for="agencyDescription"><?php _e("Description", "retxtdom");?></label>
-                    </th>
-                    <td>
-                        <?php
-                            wp_editor(
-                                $user["agencyDescription"], //Content
-                                "agencyDescription", array(
-                                    "textarea_name" => "agencyDescription",
-                                    "textarea_rows" => 10,
-                                )
-                            );
-                        ?>
-                    </td>
-                </tr>
-            </table>
-        <?php
+            
+        <div id="tableCustomProfile">
+            <input type="radio" class="tct" name="tct" id="tctAccount">
+            <input type="radio" class="tct" name="tct" id="tctBuyer" checked>
+            <input type="radio" class="tct" name="tct" id="tctGuarantor">
+            <div id="tabs">
+                <span class="tabAccount">
+                    <label for="tctAccount"><?php _e("Account", "reptxtdom"); ?></label>
+                </span>
+                <span class="tabBuyer">
+                    <label class="selectedTab" for="tctBuyer"><?php _e("Buyer", "reptxtdom"); ?> 1</label>
+                </span>
+                <span class="tabGuarantor">
+                    <label for="tctGuarantor"><?php _e("Guarantor", "reptxtdom"); ?> 1</label>
+                </span>
+            </div>
+            <div id="tabsContent">
+                <div class="contentAccount">
+                    <fieldset>
+                        <legend><?php _e("Account", "reptxtdom"); ?></legend>
+                        <!-- profileCallback() -->
+                    </fieldset>
+                </div>
+                <div class="contentBuyer defaultContent">
+                    <fieldset>
+                        <legend><?php _e("Personal situation", "reptxtdom"); ?></legend>
+                        <table class="form-table personalSituation" role="presentation">
+                            <tbody>
+                                <?php foreach($fields["buyer"]["personalSituation"] as $field) { 
+                                    self::printField($field);
+                                } ?>
+                            </tbody>
+                        </table>
+                    </fieldset>
+                    <fieldset>
+                        <legend><?php _e("Professional situation", "reptxtdom"); ?></legend>
+                        <table class="form-table professionalSituation" role="presentation">
+                            <tbody>
+                                <?php foreach($fields["buyer"]["professionalSituation"] as $field) {
+                                    self::printField($field);
+                                } ?>
+                            </tbody>
+                        </table>
+                    </fieldset>
+                    <fieldset>
+                        <legend><?php _e("Other", "reptxtdom"); ?></legend>
+                        <table class="form-table other" role="presentation">
+                            <tbody>
+                                <?php foreach($fields["buyer"]["other"] as $field) {
+                                    self::printField($field);
+                                } ?>
+                            </tbody>
+                        </table>
+                    </fieldset>
+                </div>
+                <div class="contentGuarantor">
+                    <fieldset>
+                        <legend><?php _e("Personal situation", "reptxtdom"); ?></legend>
+                        <table class="form-table personalSituation" role="presentation">
+                            <tbody>
+                                <?php foreach($fields["guarantor"]["personalSituation"] as $field) {
+                                    self::printField($field);
+                                } ?>
+                            </tbody>
+                        </table>
+                    </fieldset>
+                    <fieldset>
+                        <legend><?php _e("Professional situation", "reptxtdom"); ?></legend>
+                        <table class="form-table professionalSituation" role="presentation">
+                            <tbody>
+                                <?php foreach($fields["guarantor"]["professionalSituation"] as $field) {
+                                    self::printField($field);
+                                } ?>
+                            </tbody>
+                        </table>
+                    </fieldset>
+                    <fieldset>
+                        <legend><?php _e("Personal situation", "reptxtdom"); ?></legend>
+                        <table class="form-table personalSituation" role="presentation">
+                            <tbody>
+                                <?php foreach($fields["guarantor"]["other"] as $field) {
+                                    self::printField($field);
+                                } ?>
+                            </tbody>
+                        </table>
+                    </fieldset>
+                </div>
+                
+            </div>
+        </div>
+        <?php }        
     }
     
     
@@ -122,4 +143,44 @@ class REALM_EditProfile {
             REALM_UserModel::updateUser($idUser); //Save in BDD         
         }    
     }
+    
+    private function printField($field) { ?>
+        <tr class="customFieldWrap">
+            <th scope="row">
+                <label for="<?=$field["nameAttr"];?>"><?=$field["name"];?></label>
+            </th>
+            <td>
+                <?php
+                    switch($field["type"]) {
+                        case "text": 
+                            printf('<input type="text" name="%s" value="%s" class="regular-text"%s>', $field["nameAttr"], null, $field["optionnal"]?" required":'');
+                            break;
+                        case "date":
+                            printf('<input type="date" name="%s" value="%s" class="regular-text"%s>', $field["nameAttr"], null, $field["optionnal"]?" required":'');
+                            break;
+                        case "number":
+                            printf('<input type="number" name="%s" min="0" value="%s" class="regular-text"%s>', $field["nameAttr"], null, $field["optionnal"]?" required":'');
+                            break;
+                        case "file":
+                            printf('<input type="file" name="%s" accept="%s" value="%s" class="regular-text"%s>', $field["nameAttr"], $field["extensions"], null, $field["optionnal"]?" required":'');
+                            break;
+                        case "files":
+                            printf('<input type="file" name="%s" accept="%s" value="%s" class="regular-text" multiple%s>', $field["nameAttr"], $field["extensions"], null, $field["optionnal"]?" required":'');
+                            break;
+                        case "select":
+                            printf('<select name="%s"%s>', $field["nameAttr"], $field["optionnal"]?" required":'');
+                            foreach($field["selectValues"] as $option) {
+                                printf('<option value="%s"%s>%s</option>', str_replace(' ', '', strtolower($option)), null, $option);
+                            }
+                            ?></select><?php
+                            break;
+                    }
+
+                    if(isset($field["description"]) && !empty($field["description"])) { ?>
+                        <span class="description"></span>
+                    <?php }
+                ?>
+            </td>
+        </tr>
+    <?php }
 }
